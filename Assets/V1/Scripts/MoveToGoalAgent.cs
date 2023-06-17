b8cc4e69bc5fc25b5226c86572747aab46fde1e2
Hasi-b <hasib.hasan9@gmail.com>
2023-06-17 23:58:05 +0600

    tarining setup ready

--- Assets/V1/Scripts/MoveToGoalAgent.cs
+++ Assets/V1/Scripts/MoveToGoalAgent.cs
@@ -2,18 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class MoveToGoalAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    float moveX;
+    float moveY;
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    Transform targetTransform;
+
+
+    public override void OnEpisodeBegin()
     {
-        
+        transform.position = Vector3.zero;
+        base.OnEpisodeBegin();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void CollectObservations(VectorSensor sensor)
     {
-        
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+        base.CollectObservations(sensor);
     }
+
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+        Debug.Log(moveX+","+moveY);
+        transform.position += new Vector3(moveX,0f,moveY)*Time.deltaTime*moveSpeed;
+        base.OnActionReceived(actions);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SetReward(1f);
+        EndEpisode();
+    }
+
 }
